85a8fc5130b4ec1e6d2cbb2649485bf37b40230e
Aadit-Ambadkar <aadit.ambadkar@gmail.com>
2022-08-09 20:09:28 -0700

    PROD!!!!

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,31 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import Nav from '../components/NavBar';
+import useSystemTheme from 'react-use-system-theme';
+
 
 function MyApp({ Component, pageProps }) {
+  const systemTheme = useSystemTheme();
+
   return (
     <div>
       <Head>
         <title>Taskily Website</title>
         <meta name="description" content="Organize Your Day!" />
-        <link rel="icon" href="/favicon.ico" />
+        {
+          systemTheme !== 'dark' && (
+            <>
+              <link rel="icon" href="/taskily-ico.svg" />
+            </>
+          )
+        }
+        {
+          systemTheme === 'dark' && (
+            <>
+              <link rel="icon" href="/taskily-ico-dark.svg" />
+            </>
+          )
+        }
       </Head>
       <Nav names={['New Task', 'All Tasks', 'Clear All Tasks']} links={['/new', '/all', '/clear']}></Nav>
       <Component {...pageProps} />
